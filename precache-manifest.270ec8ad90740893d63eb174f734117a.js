3c8517ff24d5bbd12f59a7bbb040816b77c9a958
Radek Ripa <ripa.r@seznam.cz>
2019-11-18 23:49:03 +0100

    Updates

---
+++ precache-manifest.270ec8ad90740893d63eb174f734117a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8b0a688a101b83988dfafb21190d53b8",
+    "url": "/stm_crystals/index.html"
+  },
+  {
+    "revision": "4af08c0013c16337d60c",
+    "url": "/stm_crystals/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "4b1ded7bd8a936da47ac",
+    "url": "/stm_crystals/static/js/2.97e0f7f1.chunk.js"
+  },
+  {
+    "revision": "4af08c0013c16337d60c",
+    "url": "/stm_crystals/static/js/main.5c89e167.chunk.js"
+  },
+  {
+    "revision": "8cc20b147a1bc10af135",
+    "url": "/stm_crystals/static/js/runtime-main.3cb3268f.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/stm_crystals/static/media/logo.25bf045c.svg"
+  }
+]);
