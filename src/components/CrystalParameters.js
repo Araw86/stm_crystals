13409b15495ab9d6eee425d37ae5fad4a1485c86
Radek Ripa <ripa.r@seznam.cz>
2019-11-22 13:01:09 +0100

    adding card component

--- src/components/CrystalParameters.js
+++ src/components/CrystalParameters.js
@@ -1,35 +1,50 @@
 import React, { Component } from 'react';
-import { TextField, Box, FormControl, InputAdornment, Input, FormHelperText, InputLabel} from '@material-ui/core';
+import { TextField, Box, FormControl, InputAdornment, Input, InputLabel, Card, CardHeader, CardContent , Typography, FormHelperText} from '@material-ui/core';
+import { typography } from '@material-ui/system';
 
 export class CrystalParameters extends Component {
   render() {
     return (
       <Box m={1}>
-        <TextField
-          id="c0Value"
-          label="C0"
-          margin="normal"
-        />
-        <TextField
-          // hintText="Enter Your Last Name"
-          id="clValue"
-          label="Cl"
-          margin="normal"
-        />
-
-        <FormControl>
-        <InputLabel htmlFor="standard-weight-helper-text">C0 capacitance</InputLabel>
-          <Input
-            id="standard-adornment-weight"
-            endAdornment={<InputAdornment position="end">pF</InputAdornment>}
-            aria-describedby="standard-weight-helper-text"
-            inputProps={{
-              'aria-label': 'C0 capacitance',
-            }}
+        <Card>
+          <CardHeader
+            title="LSE crystal parameters"
+            subheader="sub header test"
           />
-          
-          {/* <FormHelperText id="standard-weight-helper-text">C0</FormHelperText> */}
-        </FormControl>
+          <CardContent>
+          <Typography type='p'>
+            Please fill the Loading capacitance of cystal and C0 capacitance. This values are written in the Datasheet to related crystall. 
+            Values will be used to calculate compatybility with STM32 device
+          </Typography>
+          <br/>
+            <FormControl>
+              <InputLabel htmlFor="standard-c0-helper-text">C0 capacitance</InputLabel>
+              <Input
+                id="standard-adornment-c0"
+                endAdornment={<InputAdornment position="end">pF</InputAdornment>}
+                aria-describedby="standard-c0-helper-text"
+                inputProps={{
+                  'aria-label': 'C0 capacitance',
+                }}
+              />
+              {/* <FormHelperText id="standard-c0-helper-text">Enter valid nunber</FormHelperText> */}
+            </FormControl>
+            <br />
+            <br />
+            <FormControl>
+              <InputLabel htmlFor="standard-cl-helper-text">C0 capacitance</InputLabel>
+              <Input
+                id="standard-adornment-cl"
+                endAdornment={<InputAdornment position="end">pF</InputAdornment>}
+                aria-describedby="standard-cl-helper-text"
+                inputProps={{
+                  'aria-label': 'Cl capacitance',
+                }}
+              />
+
+            </FormControl>
+          </CardContent>
+        </Card>
       </Box>
     )
   }
