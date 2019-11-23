d9a376cb4832cf8dffe8cdc1f7511ac88d71fd86
Radek Ripa <ripa.r@seznam.cz>
2019-11-23 18:27:51 +0100

    allow to store float value

--- src/components/CrystalParameters.js
+++ src/components/CrystalParameters.js
@@ -1,21 +1,43 @@
-import React, { Component, Fragment } from 'react';
-import { TextField, Box, FormControl, InputAdornment, Input, InputLabel, Card, CardHeader, CardContent, Typography, FormHelperText, FormLabel } from '@material-ui/core';
-import { typography } from '@material-ui/system';
+import React, { Component, Fragment } from "react";
+import {
+  TextField,
+  Box,
+  FormControl,
+  InputAdornment,
+  Input,
+  InputLabel,
+  Card,
+  CardHeader,
+  CardContent,
+  Typography,
+  FormHelperText,
+  FormLabel
+} from "@material-ui/core";
+import { typography } from "@material-ui/system";
 
 export class CrystalParameters extends Component {
   constructor(props) {
     super(props);
     this.state = {
       c0: 0.0,
+      c0String: "",
       cl: 0.0,
+      clString: "",
       esr: 0,
+      esrString: "",
       gmcrit: 0.0,
-      f: 32768,
+      f: 32768
     };
   }
   calculateGM() {
     let { c0, cl, esr, f } = this.state;
-    let gmcrit = 4 * esr * 1e3 * Math.pow((2 * Math.PI * f), 2) * Math.pow(((cl + c0) * 1e-12), 2)*1e6;
+    let gmcrit =
+      4 *
+      esr *
+      1e3 *
+      Math.pow(2 * Math.PI * f, 2) *
+      Math.pow((cl + c0) * 1e-12, 2) *
+      1e6;
     gmcrit = Number(gmcrit.toFixed(4));
     this.setState({ gmcrit: gmcrit });
   }
@@ -27,16 +49,22 @@ export class CrystalParameters extends Component {
 
   handleChange = input => e => {
     const value = Number(e.target.value);
-    console.log(value);
-    console.log(isNaN(value));
+    // console.log(e.target.value);
+    // console.log(value);
+    // console.log(isNaN(value));
     if (!isNaN(value)) {
       // if (typeof value == 'number') {
-      this.setState({ [input]: value }, ()=>{
-        this.calculateGM();
-      });
+      const valueToState = Number(value.toFixed(2));
+      // console.log(valueToState);
+      this.setState(
+        { [input]: valueToState, [input + "String"]: e.target.value },
+        () => {
+          this.calculateGM();
+        }
+      );
       // this.calculateGM();
     }
-  }
+  };
 
   render() {
     return (
@@ -47,62 +75,80 @@ export class CrystalParameters extends Component {
             subheader="sub header test"
           />
           <CardContent>
-            <Typography type='p'>
-              Please fill the Loading capacitance of cystal and C<sub>0</sub> capacitance. This values are written in the Datasheet to related crystall.
-              Values will be used to calculate compatybility with STM32 device
-          </Typography>
+            <Typography type="p">
+              Please fill the Loading capacitance of cystal and C<sub>0</sub>{" "}
+              capacitance. This values are written in the Datasheet to related
+              crystall. Values will be used to calculate compatybility with
+              STM32 device
+            </Typography>
             <br />
             <FormControl>
-              <InputLabel htmlFor="standard-c0-helper-text">C<sub>0</sub> capacitance</InputLabel>
+              <InputLabel htmlFor="standard-c0-helper-text">
+                C<sub>0</sub> capacitance
+              </InputLabel>
               <Input
                 id="standard-adornment-c0"
-                endAdornment={<InputAdornment position="end">pF</InputAdornment>}
+                endAdornment={
+                  <InputAdornment position="end">pF</InputAdornment>
+                }
                 aria-describedby="standard-c0-helper-text"
                 inputProps={{
-                  'aria-label': 'C0 capacitance',
+                  "aria-label": "C0 capacitance"
                 }}
-                onChange={this.handleChange('c0')}
-                value={this.state.c0}
+                onChange={this.handleChange("c0")}
+                value={this.state.c0String}
               />
               {/* <FormHelperText id="standard-c0-helper-text">Enter valid nunber</FormHelperText> */}
             </FormControl>
             <br />
             <br />
-            <FormControl >
-              <InputLabel htmlFor="standard-cl-helper-text">C<sub>L</sub> capacitance</InputLabel>
+            <FormControl>
+              <InputLabel htmlFor="standard-cl-helper-text">
+                C<sub>L</sub> capacitance
+              </InputLabel>
               <Input
                 id="standard-adornment-cl"
-                endAdornment={<InputAdornment position="end">pF</InputAdornment>}
+                endAdornment={
+                  <InputAdornment position="end">pF</InputAdornment>
+                }
                 aria-describedby="standard-cl-helper-text"
                 inputProps={{
-                  'aria-label': 'Cl capacitance',
+                  "aria-label": "Cl capacitance"
                 }}
-                onChange={this.handleChange('cl')}
-                value={this.state.cl}
+                onChange={this.handleChange("cl")}
+                value={this.state.clString}
               />
             </FormControl>
             <br />
             <br />
-            <FormControl >
+            <FormControl>
               <InputLabel htmlFor="standard-esr-helper-text">ESR</InputLabel>
               <Input
                 id="standard-adornment-esr"
-                endAdornment={<InputAdornment position="end">k&Omega;</InputAdornment>}
+                endAdornment={
+                  <InputAdornment position="end">k&Omega;</InputAdornment>
+                }
                 aria-describedby="standard-esr-helper-text"
                 inputProps={{
-                  'aria-label': 'ESR',
+                  "aria-label": "ESR"
                 }}
-                onChange={this.handleChange('esr')}
-                value={this.state.esr}
+                onChange={this.handleChange("esr")}
+                value={this.state.esrString}
               />
             </FormControl>
             <br />
             <br />
             <TextField
-              label={<Fragment>g<sub>mcrit</sub></Fragment>}
+              label={
+                <Fragment>
+                  g<sub>mcrit</sub>
+                </Fragment>
+              }
               id="standard-adornment-gmcrit"
               InputProps={{
-                endAdornment: <InputAdornment position="end">&mu;A/V</InputAdornment>,
+                endAdornment: (
+                  <InputAdornment position="end">&mu;A/V</InputAdornment>
+                ),
                 readOnly: true
               }}
               value={this.state.gmcrit}
@@ -110,7 +156,7 @@ export class CrystalParameters extends Component {
           </CardContent>
         </Card>
       </Box>
-    )
+    );
   }
 }
 
